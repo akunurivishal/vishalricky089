6678ac650974bb5758139891754af8c9869fca39
akunurivishal <58929029+akunurivishal@users.noreply.github.com>
2019-12-18 12:18:47 +0530

    Add files via upload

---
+++ main (3).c
@@ -0,0 +1,37 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+int main()
+{
+    char ch;
+    scanf("%c",&ch);
+    switch(ch)
+    {
+ case 'a':
+ case 'e':
+ case 'i':
+ case 'o':
+ case 'u':
+ case 'A':
+ case 'E':
+ case 'I':
+ case 'O':
+ case 'U':
+printf("it is a vowel");
+break;
+default:
+printf("it is consonant");
+break;
+}
+return 0;
+}
+
+
+
+
